12fff6401826c802a66131e1fa803c3d41c0b518
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 11:37:15 -0700

    Update java-8.sh

--- java-8.sh
+++ java-8.sh
@@ -11,9 +11,12 @@ tar -zxf jdk-8u5-linux-x64.tar.gz -C /opt/jdk
 
 ls /opt/jdk
 
-update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_05/bin/java 100
 
-update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_05/bin/javac 100
+#The last number 1100 is the priority mine had java 7 at 1097 online help site had these were set at 100
+
+update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_05/bin/java 1100
+
+update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_05/bin/javac 1100
 
 update-alternatives --display java
 
