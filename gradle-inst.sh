130f3534cf18f1d1b4f4bc3b1681117440fbeea3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-06 23:50:23 -0700

    Update gradle-inst.sh

--- gradle-inst.sh
+++ gradle-inst.sh
@@ -6,9 +6,9 @@ echo "Now Gradle"
 gradle_version=2.9
 
 sudo mkdir /home/ubuntu/workspace/gradle
-wget -N http://downloads.gradle.org/distributions/gradle-${gradle_version}-all.zip
-unzip -oq ./gradle-${gradle_version}-all.zip -d /home/ubuntu/workspace/gradle
-ln -sfnv gradle-${gradle_version} /home/ubuntu/workspace/gradle/gradle-${gradle_version}
+sudo wget -N http://downloads.gradle.org/distributions/gradle-${gradle_version}-all.zip
+sudo unzip -oq ./gradle-${gradle_version}-all.zip -d /home/ubuntu/workspace/gradle
+sudo ln -sfnv gradle-${gradle_version} /home/ubuntu/workspace/gradle/gradle-${gradle_version}
 
 echo "exporting the Path to my .profile file so other terminals have the path"
 
@@ -24,7 +24,7 @@ export PATH=$PATH:$GRADLE_USER_HOME/bin
 
 #printf "export GRADLE_HOME=/home/ubuntu/workspace/gradle/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin" > /etc/profile.d/gradle.sh
 #. /etc/profile.d/gradle.sh
-hash -r ; sync
+#hash -r ; sync
 # check installation
 
 echo "setup gradle"
@@ -33,7 +33,7 @@ gradle
 echo "see if it worked"
 gradle -v
 
-rm gradle-2.9-all.zip
+sudo rm gradle-2.9-all.zip
 
 echo "-----------------------All Setup-------------------------"
 echo "------------------------------------------------------------------------------"
@@ -44,12 +44,13 @@ echo "Now the fun part, Lets make the Hello World App for both Ant and Gradle"
 
 
 
+sudo chmod +x a00-auto-ant-gradle.sh
+sudo chmod +x a04-restart-webserver.sh
 
-
-./a00-auto-ant-gradle.sh
+sudo ./a00-auto-ant-gradle.sh
 
 echo "Now lets start Apache"
 
-./a04-restart-webserver.sh
+sudo ./a04-restart-webserver.sh
 
 
